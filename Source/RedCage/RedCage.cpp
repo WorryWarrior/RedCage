66812744953ce7f7a76d99b6993d5a000807f911
Dan <worrywarrior1@gmail.com>
2024-03-11 14:56:32 +0300

    Project Setup

---
+++ Source/RedCage/RedCage.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RedCage.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RedCage, "RedCage" );
+
+DEFINE_LOG_CATEGORY(LogRedCage)
+ 
